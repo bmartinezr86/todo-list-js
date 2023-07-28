56a25062b9eed23423a33734e047d0d8d12fbc4d
Bárbara Martínez Rizo <bmartinezASIR@gmail.com>
2023-07-28 15:07:42 +0200

    Funcionalidad de añadir tareas y eliminar implementada

---
+++ assets/js/main2.js
@@ -0,0 +1,67 @@
+'use strict'
+
+var formulario = document.querySelector("#formulario");
+
+// Añadir tareas al local storage
+formulario.addEventListener("submit", () => {
+    var tareasPendientes = document.querySelector("#tareas-pendientes");
+    const keyTarea = "tarea_" + (localStorage.length + 1); // genera key aleatoria
+    const tarea = document.querySelector("#input-tarea").value;
+    localStorage.setItem(keyTarea, tarea);
+});
+
+// Generar listado de tareas
+var contenedorListaTareas = document.querySelector("#tareas-pendientes");
+for (const i in localStorage) {
+    if (typeof localStorage[i] == "string") {
+        // Generar estructura del contenido
+        const divTarea = document.createElement("div");
+        divTarea.classList.add("tarea");
+
+        const spanTarea = document.createElement("span");
+        spanTarea.setAttribute("data-codigo", localStorage.key(i));
+        spanTarea.textContent = localStorage[i];
+
+
+        const divIconosModificar = document.createElement("div");
+        divIconosModificar.classList.add("iconos-modificar");
+
+        const iconoEliminar = document.createElement("span");
+        iconoEliminar.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+        const iconoEditar = document.createElement("span");
+        iconoEditar.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+
+        const iconoCheck = document.createElement("span");
+        iconoCheck.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+
+        // Agregar los elementos al contenedor de la tarea
+        divIconosModificar.appendChild(iconoEliminar);
+        divIconosModificar.appendChild(iconoEditar);
+        divIconosModificar.appendChild(iconoCheck);
+
+        divTarea.appendChild(spanTarea);
+        divTarea.appendChild(divIconosModificar);
+
+        // Agregar la tarea al contenedor de tareas
+        contenedorListaTareas.appendChild(divTarea);
+    }
+}
+
+// Eliminar tarea 
+var botonesEliminar = document.querySelectorAll(".fa-trash");
+botonesEliminar.forEach(botonEliminar => {
+    botonEliminar.addEventListener("click", () => {
+
+        const tareaAEliminar = botonEliminar.parentNode.parentNode.parentNode;
+        const tareaAEliminarSPAN = tareaAEliminar.querySelector("span").getAttribute('data-codigo');
+        console.log(tareaAEliminarSPAN);
+
+        localStorage.removeItem(tareaAEliminarSPAN); // Eliminar del localstorage
+        tareaAEliminar.remove(); // Eliminar del html
+    });
+});
+
+// Editar tarea
+
+// Finalizar tarea
